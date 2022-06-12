3db76ba30f3d2a1ac6dc6db26c2783e519dabb57
Jagoda Kubicka <93054999+jagodakubicka@users.noreply.github.com>
2022-06-12 23:16:03 +0200

    added js to new html files

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,7 +1,7 @@
 //navbar
-const toggleBtn = document.querySelectorAll('.nav__toggleBtn');
+const toggleBtn = document.querySelector('.nav__toggleBtn');
 
-const navList = document.querySelectorAll('.nav__list');
+const navList = document.querySelector('.nav__list');
 
 toggleBtn.addEventListener('click', () => {
   navList.classList.toggle('nav__list--active');
