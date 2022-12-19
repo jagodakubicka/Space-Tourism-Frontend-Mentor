7ae86ddd57554a6de8aa958645336b0170ef07df
Jagoda Kubicka <93054999+jagodakubicka@users.noreply.github.com>
2022-12-19 21:48:30 +0100

    tech page logic with data attr

--- starter-code/technology.js
+++ starter-code/technology.js
@@ -2,36 +2,25 @@
 
 
 
-function fetchData(){
-  fetch('data.json').then((res)=> res.json())
-  .then((data)=> {
-    console.log(data.technology);
-    let output = ''
-    data.technology.forEach(function (item) {
-      output += `
-      <div data-id="${item.id}" class="page-technology__contentCtn">
-        <p class="page-technology__title">The terminology...</p>
-            <p class="page-technology__mainTitle">${item.name}</p>
-            <p class="page-technology__desc">
-              ${item.description}
-            </p>
-          
-          <div class="page-technology__img">
-            <img
-              id="tech-img"
-              src=${item.images.portrait}
-              alt=""
-            />
-          </div>
-      </div>`
+const techBtns = document.querySelectorAll('.tech-btn')
+
+
+techBtns.forEach((techBtn)=>{
+  techBtn.addEventListener('click', event => {
+    let id = event.target.getAttribute('data-id');
+   
+    const techContents = document.querySelectorAll('.page-technology__content')
+    techContents.forEach((techContent)=>{
+      contentId = techContent.getAttribute('data-id');
+      if(id == contentId){
+        techContent.classList.add('active');
+        techContent.classList.remove('disabled');
+      } else{
+         techContent.classList.remove('active');
+         techContent.classList.add('disabled');
+
+      }
     })
-    document.getElementById('page-technology__content').innerHTML = output
   })
-  .catch((error) => {
-    console.log(`Error Fetching data : ${error}`)
-    document.getElementById('page-technology__content').innerHTML = 'Error Loading Data'
-  })
-
-}
-
-fetchData()
+  
+})
